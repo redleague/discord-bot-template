cbf16c0c20c8ec961a82dc9f4292329ddb8acbb2
HYDROX <40756526+inhydrox@users.noreply.github.com>
2021-05-19 20:48:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "NDc5MTQzNDYwNTE2MTM0OTIy.W3OrHw.kAizjFSkXFpKPCEu8iH5ze-ss3E",
+  token: "NDc5MTQzNDYwNTE2MTM0OTIy.W3OrHw.kAizjFSkXFpKPCEu8iH5ze-ss3E", //this token is invalid make sure you replace it with yours
   prefix: "*",
   supportServer: (code) => `https://discord.gg/${code}`,
   inviteURL: (id, permissions) => `https://discord.com/api/oauth2/authorize?client_id=${id}&permissions=${permissions ? permissions : '8'}&scope=bot`,
