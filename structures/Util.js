4f5cdc1feaf3bfd62b1722a243ad6e60e07c9661
Rajat Mishra <85543339+hydrox19@users.noreply.github.com>
2021-06-23 10:23:52 +0530

    Update Util.js

--- structures/Util.js
+++ structures/Util.js
@@ -1,8 +1,5 @@
 const suffixes = ["Bytes", "KB", "MB", "GB"];
 
-const moment = require("moment-timezone");
-require("moment");
-
 module.exports = class Util {
   constructor(client) {
     this.client = client;
@@ -24,9 +21,8 @@ module.exports = class Util {
   
   get emoji() {
     return {
-      success: "<a:checkmark:755254019689283594>",
-      error: "<a:crossmark:755253920934133879>",
-      search: "🔍",
+      success: "✅",
+      error: "❌",
       warning: "⚠️",
     }
   }
@@ -35,28 +31,10 @@ module.exports = class Util {
     let color = Math.floor(Math.random()*16777215).toString(16);
     return `${parseInt(color, 16)}`;
   }
-
-  get getTime() {
-    return moment(new Date()).tz('Asia/Kolkata').format('dddd MMMM Do h:mm A');
-  }
   
   toProperCase(str) {
     return str.replace(/([^\W_]+[^\s-]*) */g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
   }
-
-  shuffle(array) {
-    let tmp;
-    let current;
-    let top = array.length;
-
-    if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-    }
-    return array;
-}
 
   random(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -66,29 +44,9 @@ module.exports = class Util {
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
     return (!bytes && "0 Bytes") || `${(bytes / Math.pow(1024, i)).toFixed(2)} ${suffixes[i]}`;
   }
-  
-  codeBlock(string, code) {
-    if(code) return `\`\`\`${code}\n${string}\`\`\``;
-    return `\`\`\`${string}\`\`\``;
-  };
-  
-  async haste(text) {
-     const req = await this.client.snek.post("https://haste.ntmnathan.com/documents", { text });
-     return `https://haste.ntmnathan.com/${req.data.key}`   
-  };
-  
+
   escapeRegex(str) {
     return str.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
-  }
-  
-  // Convert milliseconds into human readable string.
-  getDuration(time) {
-    const seconds = Math.floor(time / 1000) % 60 ;
-    const minutes = Math.floor((time / (1000 * 60)) % 60);
-    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-    const days = Math.floor((time / (1000 * 60 * 60 * 24)) % 7);
-    return [`${days} Days`, `${hours} Hours`, `${minutes} Minutes`,
-      `${seconds} Seconds`].filter((time) => !time.startsWith("0")).join(", ");
   }
 
   formatSeconds (time, yt = false) {
@@ -113,10 +71,6 @@ module.exports = class Util {
   delay(ms) {
 		return new Promise(res => setTimeout(res, ms));
 	}
-  
-  randomNum(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
 
   async fetchChannel(channelID) {
     return await this.client.channels.fetch(channelID).catch(() => false);
