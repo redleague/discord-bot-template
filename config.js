f5920199d46d89daee86bd6dae2f957bff3a90a0
Rajat Mishra <85543339+hydrox19@users.noreply.github.com>
2021-08-27 14:12:20 +0000

    minor

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "ODYyNTczNTIxMDA3NTQyMzEy.YOaUMQ.KjXPDnlMeD8Z7Cy41uzGx6EtyGU", //this token is invalid make sure you replace it with yours
+  token: "00000000000000000000000000000", //this token is invalid make sure you replace it with yours
   prefix: "*",
   supportServer: (code) => `https://discord.gg/${code}`,
   inviteURL: (id, permissions) => `https://discord.com/api/oauth2/authorize?client_id=${id}&permissions=${permissions ? permissions : '8'}&scope=bot`,
