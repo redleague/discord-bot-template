34d924ac6ac0a852da36cfbd51bec429ed21052d
Rajat Mishra <85543339+hydrox19@users.noreply.github.com>
2021-08-27 14:32:43 +0000

    minor stuff

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: "00000000000000000000000000000", //this token is invalid make sure you replace it with yours
   prefix: "*",
-  dev: true,
+  dev: true, //if the bot is not in production
   supportServer: (code) => `https://discord.gg/${code}`,
   inviteURL: (id, permissions) => `https://discord.com/api/oauth2/authorize?client_id=${id}&permissions=${permissions ? permissions : '8'}&scope=bot`,
 }
