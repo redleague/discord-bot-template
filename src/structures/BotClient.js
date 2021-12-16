f6990c31b260e0225491987694e6b239c6dbb409
hydrox19 <hydrox19@yandex.com>
2021-12-16 14:10:25 +0530

    refactor(BotClient): refactoring

--- src/structures/BotClient.js
+++ src/structures/BotClient.js
@@ -1,16 +1,23 @@
 import { Client } from "discord.js";
 import { config } from "../../config";
 import { createLogger } from "../modules/Logger";
+import { CommandManager } from "../modules/CommandManager";
+import { EventManager } from "../modules/EventManager";
 
 export class BotClient extends Client {
     constructor(options) {
         super(options);
 
         this.config = config;
-        this.logger = createLogger;
+        this.logger = createLogger("main", "en-US", "shard", this.shard?.ids[0], this.config.isDev);
+        this.commands = new CommandManager(this, resolve(__dirname, "..", "commands"));
+        this.events = new EventManager(this, this, resolve(__dirname, "..", "events"));
     }
 
     async build() {
+        this.events.load();
+        this.on("ready", () => this.commands.load());
         await this.login(this.config.token);
+        return this;
     }
 }
