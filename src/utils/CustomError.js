942bf11f9913ee541e2609b9ea2a71234ddc6d07
hydrox19 <hydrox19@yandex.com>
2021-12-16 10:41:39 +0530

    feat(utils/CustomError): create custom error function

---
+++ src/utils/CustomError.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {string} name 
+ * @param {string|undefined} message 
+ * @returns Error
+ */
+
+export function CustomError(name, message) {
+    const error = new Error(message);
+    error.name = name;
+    return error;
+}
