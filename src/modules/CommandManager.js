0c45bce104b10f03e4e4ca579444f1135cd2a576
hydrox19 <hydrox19@yandex.com>
2021-12-22 20:53:00 +0530

    refactor: CommandManager

--- src/modules/CommandManager.js
+++ src/modules/CommandManager.js
@@ -13,42 +13,33 @@ export class CommandManager extends Collection {
         this.isReady = false;
     }
 
-    load() {
-        fs.readdir(resolve(this.path))
-            .then(async categories => {
-                for (const category of categories) {
-                    await fs.readdir(resolve(this.path, category))
-                        .then(async files => {
-                            const allCmd = await this.client.application.commands.fetch().catch(() => this.client.logger.info(new CustomError("GLOBAL_SLASH_FETCH", "An error occured while fetching slash comamnds")));
-                            for (const file of files) {
-                                const path = resolve(this.path, category, file);
-                                const command = await this.import(path, this.client, { category, path });
-                                if (command === undefined) throw new Error(`Command: ${file} is not a valid command`);
-                                command.meta = Object.assign(command.meta, { path, category });
-                                this.set(command.meta.slash.name, command);
-                                if (!allCmd.find(c => c.name !== command.meta.slash.name)) {
-                                    if (this.client.config.isDev) {
-                                        for (const devGuild of this.client.config.devGuilds) {
-                                            const guild = await this.client.guilds.fetch(devGuild);
-                                            await guild.commands.create(command.meta.slash)
-                                                .catch(() => this.client.logger.info(`Missing access on ${guild} [SLASH_COMMAND]`));
-                                        }
-                                    } else {
-                                        await this.client.application.commands.create(command.meta.slash).catch(() => this.client.logger.log(new CustomError("GLOBAL_SLASH_REGISTER", "Error in registering global slash")));
-                                    }
-                                }
-                            }
-                            return { files };
-                        })
-                        .catch(err => this.client.logger.error(CustomError("CMD_LOADER_ERR:", err)))
-                        .finally(() => this.client.logger.info(`Done registering ${category} category.`));
+    async load() {
+        const categories = await fs.readdir(resolve(this.path)).catch(err => this.client.logger.error(CustomError("CATEGORY_LOADER_ERR:", err)));
+        for (const category of categories) {
+            const files = await fs.readdir(resolve(this.path, category)).catch(err => this.client.logger.error(CustomError("CMD_LOADER_ERR:", err)));
+            const allCmd = await this.client.application.commands.fetch().catch(() => this.client.logger.info(new CustomError("GLOBAL_SLASH_FETCH", "An error occured while fetching slash comamnds")));
+            for (const file of files) {
+                const path = resolve(this.path, category, file);
+                const command = await this.import(path, this.client, { category, path });
+                if (command === undefined) throw new Error(`Command: ${file} is not a valid command`);
+                command.meta = Object.assign(command.meta, { path, category });
+                this.set(command.meta.slash.name, command);
+                if (!allCmd.find(c => c.name !== command.meta.slash.name)) {
+                    if (this.client.config.isDev) {
+                        for (const devGuild of this.client.config.devGuilds) {
+                            const guild = await this.client.guilds.fetch(devGuild);
+                            await guild.commands.create(command.meta.slash)
+                                .catch(() => this.client.logger.info(`Missing access on ${guild} [SLASH_COMMAND]`));
+                        }
+                    } else {
+                        await this.client.application.commands.create(command.meta.slash).catch(() => this.client.logger.log(new CustomError("GLOBAL_SLASH_REGISTER", "Error in registering global slash")));
+                    }
                 }
-            })
-            .catch(err => this.client.logger.error(CustomError("CATEGORY_LOADER_ERR:", err)))
-            .finally(() => {
-                this.client.logger.info("All categories has been registered.");
-                this.isReady = true;
-            });
+            }
+            this.client.logger.log("CMD_LOAD_CATEGORY", `Registered ${category} category`);
+        }
+        this.client.logger.log("CMD_LOAD", "Registered all categories");
+        this.isReady = true;
     }
 
     /**
