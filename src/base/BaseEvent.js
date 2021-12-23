6504bbfb78b18230d0d28680a90d1a43f2081529
hydrox19 <hydrox19@yandex.com>
2021-12-23 11:07:50 +0530

    fix: add more checkings

--- src/base/BaseEvent.js
+++ src/base/BaseEvent.js
@@ -2,5 +2,7 @@ export class BaseEvent {
     constructor(client, name) {
         this.client = client;
         this.name = name;
+        if (this.execute === undefined) throw new TypeError(`Class ${this.constructor.name} must have a execute method`);
+        if (this.execute.constructor.name !== "AsyncFunction") throw new TypeError(`Class ${this.constructor.name} must have a asynchronus execute method`);
     }
 }
